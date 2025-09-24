7730ced334a9aa056452c21bdad292ccb42bc5b2
raulnunezpena-svg <raul.nunezpena@g.austincc.edu>
2025-09-24 13:45:44 +0000

    def main

--- src/homework/c_decisions/main.py
+++ src/homework/c_decisions/main.py
@@ -1,1 +1,8 @@
-#
+from src.homework.c_decisions.decisions import get_letter_grade
+
+def main():
+    print("Main Menu")
+    print("1. Get Letter Grade")
+    print("2.letter grade using switch")
+    priny("3. Exit")
+    
