37a58fdec4bf137d56ae1a4562f1a62e098eefc7
raulnunezpena-svg <raul.nunezpena@g.austincc.edu>
2025-11-23 22:44:03 +0000

    test 2

--- tests/homework/h_strings/tests_strings.py
+++ tests/homework/h_strings/tests_strings.py
@@ -6,13 +6,13 @@ from src.homework.h_strings.strings import get_dna_complement
 class Test_Config(unittest.TestCase):
 
     def test_get_hamming_distance(self):
-        dna1 = "GAGCCTACTAACGGGAT"
-        dna2 = "CATCGTAATGACGGCCT"
-        self.assertEqual(get_hamming_distance(dna1, dna2), 7)
+        dna1 = "AAAA"
+        dna2 = "AAAT"
+        self.assertEqual(get_hamming_distance(dna1, dna2), 1)
 
     def test_get_dna_complement(self):
-        dna = "AAAACCCGGT"
-        self.assertEqual(get_dna_complement(dna), "ACCGGGTTTT")
+        dna = "AAGT"
+        self.assertEqual(get_dna_complement(dna), "ACTT")
 
 
 if __name__ == "__main__":
