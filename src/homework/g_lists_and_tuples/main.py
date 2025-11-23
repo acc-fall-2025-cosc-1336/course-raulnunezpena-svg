2aefde03675578804b8b315e49c520b7ce2b8cb1
raulnunezpena-svg <raul.nunezpena@g.austincc.edu>
2025-11-23 23:46:35 +0000

    main

--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,1 +1,23 @@
-#
+from src.homework.g_lists_and_tuples import get_lowest_list_value, get_highest_list_value
+
+def run_option_1()
+    value = []
+
+    while True:
+        user_input = input("enter a list value")
+
+        try: 
+            num= float(user_input)
+            value.append(num)
+        except ValueError:
+            print("enter a valid number")
+            continue
+
+        if len(value)>=3:
+            again = input("do you want to add more values? (y/n): ")
+            if again.lower() != 'y':
+                break
+            
+        
+
+
