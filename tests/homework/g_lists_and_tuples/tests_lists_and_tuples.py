e753d8013775719787369889718b2b8bed467a79
raulnunezpena-svg <raul.nunezpena@g.austincc.edu>
2025-11-24 14:28:03 +0000

    test

--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,1 +1,16 @@
-#
+import unittest
+from src.homework.g_lists_and_tuples.lists import get_lowest_list_value
+from src.homework.g_lists_and_tuples.lists import get_highest_list_value
+
+class Test_Config(unittest.TestCase):
+
+    def test_get_lowest_list_value(self):
+        values = [3, 1, 4, 1, 5]
+        self.assertEqual(get_lowest_list_value(values), 1)
+
+    def test_get_highest_list_value(self):
+        values = [3, 1, 4, 1, 5]
+        self.assertEqual(get_highest_list_value(values), 5)
+if __name__ == "__main__":
+    unittest.main()
+    
