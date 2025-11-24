efb44472d85fd949d6dd131135d88a435dc1e397
raulnunezpena-svg <raul.nunezpena@g.austincc.edu>
2025-11-24 14:23:18 +0000

    Fixed indent def main

--- src/homework/g_lists_and_tuples/main.py
+++ src/homework/g_lists_and_tuples/main.py
@@ -1,49 +1,52 @@
-from lists import get_lowest_list_value
-from lists import get_highest_list_value
+from lists import get_lowest_list_value, get_highest_list_value
 
 def run_option_1():
-    value = []
+    values = []
 
     while True:
-        user_input = input("enter a list value")
+        user_input = input("Enter a list value: ")
 
-        try: 
-            num= float(user_input)
-            value.append(num)
+        try:
+            num = float(user_input)
+            values.append(num)
         except ValueError:
-            print("enter a valid number")
+            print("Enter a valid number")
             continue
 
-        if len(value)>=3:
-            again = input("do you want to add more values? (y/n): ")
+        if len(values) >= 3:
+            again = input("Do you want to add more values? (y/n): ")
             if again.lower() != 'y':
                 break
 
-            lowest = get_lowest_list_value(value)
-            highest = get_highest_list_value(value)
+    if values:
+        lowest = get_lowest_list_value(values)
+        highest = get_highest_list_value(values)
 
-            print("list values:", value)
-            print("lowest value:", lowest)
-            print("highest value:", highest)
-            
-            def main():
-                while True:
-                    print("Select an option:")
-                    print("1. Enter list values to find lowest and highest")
-                    print("2. Exit")
+        print("List values:", values)
+        print("Lowest value:", lowest)
+        print("Highest value:", highest)
+    else:
+        print("No values entered.")
 
-                    choice = input("Enter your choice (1 or 2): ")
+def main():
+    while True:
+        print("Select an option:")
+        print("1. Enter list values to find lowest and highest")
+        print("2. Exit")
 
-                    if choice == '1':
-                        run_option_1()
-                    elif choice == '2':
-                        print("Exiting the program.")
-                        break
-                    else:
-                        print("Invalid choice. Please enter 1 or 2.")
+        choice = input("Enter your choice (1 or 2): ")
 
-    if __name__ == "__main__":
-        main()
+        if choice == '1':
+            run_option_1()
+        elif choice == '2':
+            print("Exiting the program.")
+            break
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
+if __name__ == "__main__":
+    main()
+
 
 
         
