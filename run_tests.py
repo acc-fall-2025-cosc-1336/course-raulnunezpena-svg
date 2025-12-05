88cbcd898982ba7b6391bc6d7aa920f60dff7e41
raulnunezpena-svg <raul.nunezpena@g.austincc.edu>
2025-12-05 02:56:17 +0000

    unit test

--- run_tests.py
+++ run_tests.py
@@ -1,7 +1,10 @@
+# run_tests.py
+
 import unittest
-from tests.homework.g_lists_and_tuples import tests_lists_and_tuples as tests_strings
+from tests.homework.g_lists_and_tuples import tests_lists_and_tuples
 
 if __name__ == "__main__":
-    suite = unittest.TestLoader().loadTestsFromModule(tests_strings)
+    suite = unittest.TestLoader().loadTestsFromModule(tests_lists_and_tuples)
     unittest.TextTestRunner().run(suite)
-    
+
+
--- tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+++ tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
@@ -1,17 +1,40 @@
+# tests/homework/g_lists_and_tuples/tests_lists_and_tuples.py
+
 import unittest
-from src.homework.g_lists_and_tuples.lists import get_lowest_list_value
-from src.homework.g_lists_and_tuples.lists import get_highest_list_value
+from src.homework.g_lists_and_tuples.lists import get_p_distance, get_p_distance_matrix
 
-class Test_Config(unittest.TestCase):
 
-    def test_get_lowest_list_value(self):
-        values = [3, 1, 4, 1, 5]
-        self.assertEqual(get_lowest_list_value(values), 1)
+class TestListsAndTuples(unittest.TestCase):
 
-    
-    def test_get_highest_list_value(self):
-        values = [3, 1, 4, 1, 5]
-        self.assertEqual(get_highest_list_value(values), 5)
+    def test_get_p_distance(self):
+        list1 = ['T','T','T','C','C','A','T','T','T','A']
+        list2 = ['G','A','T','T','C','A','T','T','T','C']
+
+        # from the assignment: these should have p-distance 0.4
+        expected = 0.4
+        self.assertAlmostEqual(get_p_distance(list1, list2), expected)
+
+    def test_get_p_distance_matrix(self):
+        dna_lists = [
+            ['T','T','T','C','C','A','T','T','T','A'],
+            ['G','A','T','T','C','A','T','T','T','C'],
+            ['T','T','T','C','C','A','T','T','T','T'],
+            ['G','T','T','C','C','A','T','T','T','A']
+        ]
+
+        expected_matrix = [
+            [0.0, 0.4, 0.1, 0.1],
+            [0.4, 0.0, 0.4, 0.3],
+            [0.1, 0.4, 0.0, 0.2],
+            [0.1, 0.3, 0.2, 0.0]
+        ]
+
+        result = get_p_distance_matrix(dna_lists)
+
+        # compare each cell with a small tolerance
+        for i in range(len(expected_matrix)):
+            for j in range(len(expected_matrix[i])):
+                self.assertAlmostEqual(result[i][j], expected_matrix[i][j], places=5)
 
 
 if __name__ == "__main__":
